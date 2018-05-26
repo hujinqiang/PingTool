7556ad323e517b14fb91ba5a597a8a31d87f6d04
hujinqiang <854024269@qq.com>
2018-05-26 22:19:30 +0800

    TCP部分开发

---
+++ app/src/main/java/pingtool/patrol/tocel/com/pingtool/TcpSenderReceiver.java
@@ -0,0 +1,27 @@
+package pingtool.patrol.tocel.com.pingtool;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.net.ServerSocketFactory;
+import javax.net.SocketFactory;
+
+public class TcpSenderReceiver {
+
+    /**
+     * 本地serversocket默认端口,即对方连接时使用的端口号
+     */
+    public static final int LOCAL_DEFAULT_PORT = 99999;
+    private ServerSocket serverSocket;
+    private  Socket socket;
+
+    public TcpSenderReceiver(String host) {
+        try {
+            socket = SocketFactory.getDefault().createSocket(host,LOCAL_DEFAULT_PORT);
+            serverSocket = ServerSocketFactory.getDefault().createServerSocket(LOCAL_DEFAULT_PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/pingtool/patrol/tocel/com/pingtool/UDPActivity.java
+++ app/src/main/java/pingtool/patrol/tocel/com/pingtool/UDPActivity.java
@@ -1,7 +1,70 @@
 package pingtool.patrol.tocel.com.pingtool;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class UDPActivity extends Activity{
+public class UDPActivity extends Activity implements View.OnClickListener ,Handler.Callback{
+    UDPSenderReceiver senderReceiver ;
+    private EditText remoteIP;
+    private EditText remotePort;
 
+    Handler handler;
+    private EditText statics;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_udp);
+        remoteIP = findViewById(R.id.et_remote_ip);
+        remotePort = findViewById(R.id.et_remote_port);
+
+        statics = findViewById(R.id.recv_pkg_statics);
+
+        Button start = findViewById(R.id.bt_start);
+
+        handler = new Handler(this);
+
+        start.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String ip = remoteIP.getText().toString();
+        int port = Integer.parseInt(remotePort.getText().toString());
+
+        senderReceiver = new UDPSenderReceiver(ip,port);
+
+
+        new Thread(new UdpSender(senderReceiver,handler)).start();
+
+        new Thread(new UdpReceiver(senderReceiver,handler)).start();
+
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        int order = msg.arg1;
+        switch (msg.what){
+            case UDPSenderReceiver.MSG_SEND:
+                String s = "发送第" + order +"个包";
+                String newS = statics.getText().append("\n").append(s).toString();
+                statics.setText(newS);
+                break;
+
+            case UDPSenderReceiver.MSG_RECEIVE:
+                s = "接收第" + order +"个包";
+                newS = statics.getText().append("\n").append(s).toString();
+                statics.setText(newS);
+                break;
+        }
+
+        return true;
+    }
 }
